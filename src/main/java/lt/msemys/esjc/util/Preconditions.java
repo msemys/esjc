415b86371f525ebe404199dd508b6caeae51fd3d
Mindaugas Semys <mindaugas.semys@gmail.com>
2015-12-16 23:19:56 +0200

    added: 'checkState' method + formatted error message support to 'checkNotNull' and  'checkArgument' methods

--- src/main/java/lt/msemys/esjc/util/Preconditions.java
+++ src/main/java/lt/msemys/esjc/util/Preconditions.java
@@ -9,10 +9,28 @@ public class Preconditions {
         return reference;
     }
 
+    public static <T> T checkNotNull(T reference, String errorMessage, Object... errorMessageArgs) {
+        return checkNotNull(reference, String.format(errorMessage, errorMessageArgs));
+    }
+
     public static void checkArgument(boolean expression, String errorMessage) {
         if (!expression) {
             throw new IllegalArgumentException(errorMessage);
         }
+    }
+
+    public static void checkArgument(boolean expression, String errorMessage, Object... errorMessageArgs) {
+        checkArgument(expression, String.format(errorMessage, errorMessageArgs));
+    }
+
+    public static void checkState(boolean expression, String errorMessage) {
+        if (!expression) {
+            throw new IllegalStateException(errorMessage);
+        }
+    }
+
+    public static void checkState(boolean expression, String errorMessage, Object... errorMessageArgs) {
+        checkState(expression, String.format(errorMessage, errorMessageArgs));
     }
 
 }
