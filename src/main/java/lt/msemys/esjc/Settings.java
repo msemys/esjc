2f8dcf93c6c055a36c74aede833f3f5ad7a7d879
Mindaugas Semys <mindaugas.semys@gmail.com>
2016-01-17 19:38:46 +0200

    added: enriched settings

--- src/main/java/lt/msemys/esjc/Settings.java
+++ src/main/java/lt/msemys/esjc/Settings.java
@@ -1,54 +1,120 @@
 package lt.msemys.esjc;
 
-import java.net.InetSocketAddress;
-import java.time.Duration;
+import lt.msemys.esjc.node.cluster.ClusterNodeSettings;
+import lt.msemys.esjc.node.static_.StaticNodeSettings;
+import lt.msemys.esjc.operation.UserCredentials;
+import lt.msemys.esjc.tcp.TcpSettings;
 
-import static lt.msemys.esjc.util.Preconditions.checkNotNull;
+import java.time.Duration;
+import java.util.Optional;
+
+import static lt.msemys.esjc.util.Preconditions.checkArgument;
 
 public class Settings {
 
-    public final InetSocketAddress address;
+    public static final int MAX_READ_SIZE = 4 * 1024;
+
+    public final TcpSettings tcpSettings;
+    public final Optional<StaticNodeSettings> staticNodeSettings;
+    public final Optional<ClusterNodeSettings> clusterNodeSettings;
+    public final boolean ssl;
     public final Duration reconnectionDelay;
-    public final int writeBufferLowWaterMark;
-    public final int writeBufferHighWaterMark;
     public final Duration heartbeatInterval;
+    public final Duration heartbeatTimeout;
+    public final boolean requireMaster;
+    public final Optional<UserCredentials> userCredentials;
+    public final Duration operationTimeout;
+    public final Duration operationTimeoutCheckInterval;
+    public final int maxQueueSize;
+    public final int maxConcurrentOperations;
+    public final int maxOperationRetries;
+    public final int maxReconnections;
+    public final boolean failOnNoServerResponse;
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Settings{");
+        sb.append("tcpSettings=").append(tcpSettings);
+        sb.append(", staticNodeSettings=").append(staticNodeSettings);
+        sb.append(", clusterNodeSettings=").append(clusterNodeSettings);
+        sb.append(", ssl=").append(ssl);
+        sb.append(", reconnectionDelay=").append(reconnectionDelay);
+        sb.append(", heartbeatInterval=").append(heartbeatInterval);
+        sb.append(", heartbeatTimeout=").append(heartbeatTimeout);
+        sb.append(", requireMaster=").append(requireMaster);
+        sb.append(", userCredentials=").append(userCredentials);
+        sb.append(", operationTimeout=").append(operationTimeout);
+        sb.append(", operationTimeoutCheckInterval=").append(operationTimeoutCheckInterval);
+        sb.append(", maxQueueSize=").append(maxQueueSize);
+        sb.append(", maxConcurrentOperations=").append(maxConcurrentOperations);
+        sb.append(", maxOperationRetries=").append(maxOperationRetries);
+        sb.append(", maxReconnections=").append(maxReconnections);
+        sb.append(", failOnNoServerResponse=").append(failOnNoServerResponse);
+        sb.append('}');
+        return sb.toString();
+    }
 
     private Settings(Builder builder) {
-        address = builder.address;
+        tcpSettings = builder.tcpSettings;
+        staticNodeSettings = Optional.ofNullable(builder.staticNodeSettings);
+        clusterNodeSettings = Optional.ofNullable(builder.clusterNodeSettings);
+        ssl = builder.ssl;
         reconnectionDelay = builder.reconnectionDelay;
-        writeBufferLowWaterMark = builder.writeBufferLowWaterMark;
-        writeBufferHighWaterMark = builder.writeBufferHighWaterMark;
         heartbeatInterval = builder.heartbeatInterval;
+        heartbeatTimeout = builder.heartbeatTimeout;
+        requireMaster = builder.requireMaster;
+        userCredentials = Optional.ofNullable(builder.userCredentials);
+        operationTimeout = builder.operationTimeout;
+        operationTimeoutCheckInterval = builder.operationTimeoutCheckInterval;
+        maxQueueSize = builder.maxQueueSize;
+        maxConcurrentOperations = builder.maxConcurrentOperations;
+        maxOperationRetries = builder.maxOperationRetries;
+        maxReconnections = builder.maxReconnections;
+        failOnNoServerResponse = builder.failOnNoServerResponse;
     }
 
     public static Builder newBuilder() {
         return new Builder();
     }
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Settings{");
-        sb.append("address=").append(address);
-        sb.append(", reconnectionDelay=").append(reconnectionDelay);
-        sb.append(", writeBufferLowWaterMark=").append(writeBufferLowWaterMark);
-        sb.append(", writeBufferHighWaterMark=").append(writeBufferHighWaterMark);
-        sb.append(", heartbeatInterval=").append(heartbeatInterval);
-        sb.append('}');
-        return sb.toString();
-    }
-
     public static class Builder {
-        private InetSocketAddress address;
+        private TcpSettings tcpSettings;
+        private StaticNodeSettings staticNodeSettings;
+        private ClusterNodeSettings clusterNodeSettings;
+        private Boolean ssl;
         private Duration reconnectionDelay;
-        private Integer writeBufferLowWaterMark;
-        private Integer writeBufferHighWaterMark;
         private Duration heartbeatInterval;
+        private Duration heartbeatTimeout;
+        private Boolean requireMaster;
+        private UserCredentials userCredentials;
+        private Duration operationTimeout;
+        private Duration operationTimeoutCheckInterval;
+        private Integer maxQueueSize;
+        private Integer maxConcurrentOperations;
+        private Integer maxOperationRetries;
+        private Integer maxReconnections;
+        private Boolean failOnNoServerResponse;
 
         private Builder() {
         }
 
-        public Builder address(String host, int port) {
-            this.address = new InetSocketAddress(host, port);
+        public Builder tcpSettings(TcpSettings tcpSettings) {
+            this.tcpSettings = tcpSettings;
+            return this;
+        }
+
+        public Builder nodeSettings(StaticNodeSettings staticNodeSettings) {
+            this.staticNodeSettings = staticNodeSettings;
+            return this;
+        }
+
+        public Builder nodeSettings(ClusterNodeSettings clusterNodeSettings) {
+            this.clusterNodeSettings = clusterNodeSettings;
+            return this;
+        }
+
+        public Builder ssl(boolean ssl) {
+            this.ssl = ssl;
             return this;
         }
 
@@ -57,38 +123,117 @@ public class Settings {
             return this;
         }
 
-        public Builder writeBufferLowWaterMark(int size) {
-            this.writeBufferLowWaterMark = size;
-            return this;
-        }
-
-        public Builder writeBufferHighWaterMark(int size) {
-            this.writeBufferHighWaterMark = size;
-            return this;
-        }
-
         public Builder heartbeatInterval(Duration heartbeatInterval) {
             this.heartbeatInterval = heartbeatInterval;
             return this;
         }
 
+        public Builder heartbeatTimeout(Duration heartbeatTimeout) {
+            this.heartbeatTimeout = heartbeatTimeout;
+            return this;
+        }
+
+        public Builder requireMaster(boolean requireMaster) {
+            this.requireMaster = requireMaster;
+            return this;
+        }
+
+        public Builder userCredentials(String username, String password) {
+            this.userCredentials = new UserCredentials(username, password);
+            return this;
+        }
+
+        public Builder operationTimeout(Duration operationTimeout) {
+            this.operationTimeout = operationTimeout;
+            return this;
+        }
+
+        public Builder operationTimeoutCheckInterval(Duration operationTimeoutCheckInterval) {
+            this.operationTimeoutCheckInterval = operationTimeoutCheckInterval;
+            return this;
+        }
+
+        public Builder maxQueueSize(int maxQueueSize) {
+            this.maxQueueSize = maxQueueSize;
+            return this;
+        }
+
+        public Builder maxConcurrentOperations(int maxConcurrentOperations) {
+            this.maxConcurrentOperations = maxConcurrentOperations;
+            return this;
+        }
+
+        public Builder maxOperationRetries(int maxOperationRetries) {
+            this.maxOperationRetries = maxOperationRetries;
+            return this;
+        }
+
+        public Builder maxReconnections(int maxReconnections) {
+            this.maxReconnections = maxReconnections;
+            return this;
+        }
+
+        public Builder failOnNoServerResponse(boolean failOnNoServerResponse) {
+            this.failOnNoServerResponse = failOnNoServerResponse;
+            return this;
+        }
+
         public Settings build() {
-            checkNotNull(address, "address is not specified");
+            checkArgument(staticNodeSettings != null || clusterNodeSettings != null, "Missing node settings");
+            checkArgument(staticNodeSettings == null || clusterNodeSettings == null, "Usage of 'static' and 'cluster' settings at once is not allowed");
+
+            if (tcpSettings == null) {
+                tcpSettings = TcpSettings.newBuilder().build();
+            }
+
+            if (ssl == null) {
+                ssl = false;
+            }
 
             if (reconnectionDelay == null) {
                 reconnectionDelay = Duration.ofSeconds(1);
             }
 
-            if (writeBufferLowWaterMark == null) {
-                writeBufferLowWaterMark = 8 * 1024;
-            }
-
-            if (writeBufferHighWaterMark == null) {
-                writeBufferHighWaterMark = 32 * 1024;
-            }
-
             if (heartbeatInterval == null) {
                 heartbeatInterval = Duration.ofMillis(500);
+            }
+
+            if (heartbeatTimeout == null) {
+                heartbeatTimeout = Duration.ofMillis(1500);
+            }
+
+            if (requireMaster == null) {
+                requireMaster = true;
+            }
+
+            if (operationTimeout == null) {
+                operationTimeout = Duration.ofSeconds(7);
+            }
+
+            if (operationTimeoutCheckInterval == null) {
+                operationTimeoutCheckInterval = Duration.ofSeconds(1);
+            }
+
+            if (maxQueueSize == null) {
+                maxQueueSize = 5000;
+            } else {
+                checkArgument(maxQueueSize > 0, "maxQueueSize should be positive");
+            }
+
+            if (maxConcurrentOperations == null) {
+                maxConcurrentOperations = 5000;
+            }
+
+            if (maxOperationRetries == null) {
+                maxOperationRetries = 10;
+            }
+
+            if (maxReconnections == null) {
+                maxReconnections = 10;
+            }
+
+            if (failOnNoServerResponse == null) {
+                failOnNoServerResponse = false;
             }
 
             return new Settings(this);
---
+++ src/main/java/lt/msemys/esjc/tcp/TcpSettings.java
@@ -0,0 +1,144 @@
+package lt.msemys.esjc.tcp;
+
+import java.time.Duration;
+
+public class TcpSettings {
+    public final Duration connectTimeout;
+    public final Duration closeTimeout;
+    public final boolean keepAlive;
+    public final boolean tcpNoDelay;
+    public final int sendBufferSize;
+    public final int receiveBufferSize;
+    public final int writeBufferLowWaterMark;
+    public final int writeBufferHighWaterMark;
+
+    private TcpSettings(Builder builder) {
+        connectTimeout = builder.connectTimeout;
+        closeTimeout = builder.closeTimeout;
+        keepAlive = builder.keepAlive;
+        tcpNoDelay = builder.tcpNoDelay;
+        sendBufferSize = builder.sendBufferSize;
+        receiveBufferSize = builder.receiveBufferSize;
+        writeBufferHighWaterMark = builder.writeBufferHighWaterMark;
+        writeBufferLowWaterMark = builder.writeBufferLowWaterMark;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("TcpSettings{");
+        sb.append("connectTimeout=").append(connectTimeout);
+        sb.append(", closeTimeout=").append(closeTimeout);
+        sb.append(", keepAlive=").append(keepAlive);
+        sb.append(", tcpNoDelay=").append(tcpNoDelay);
+        sb.append(", sendBufferSize=").append(sendBufferSize);
+        sb.append(", receiveBufferSize=").append(receiveBufferSize);
+        sb.append(", writeBufferLowWaterMark=").append(writeBufferLowWaterMark);
+        sb.append(", writeBufferHighWaterMark=").append(writeBufferHighWaterMark);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public static Builder newBuilder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Duration connectTimeout;
+        private Duration closeTimeout;
+        private Boolean keepAlive;
+        private Boolean tcpNoDelay;
+        private Integer sendBufferSize;
+        private Integer receiveBufferSize;
+        private Integer writeBufferHighWaterMark;
+        private Integer writeBufferLowWaterMark;
+
+        public Builder connectTimeout(Duration connectTimeout) {
+            this.connectTimeout = connectTimeout;
+            return this;
+        }
+
+        public Builder closeTimeout(Duration closeTimeout) {
+            this.closeTimeout = closeTimeout;
+            return this;
+        }
+
+        public Builder keepAlive(boolean keepAlive) {
+            this.keepAlive = keepAlive;
+            return this;
+        }
+
+        public Builder tcpNoDelay(boolean tcpNoDelay) {
+            this.tcpNoDelay = tcpNoDelay;
+            return this;
+        }
+
+        public Builder sendBufferSize(int sendBufferSize) {
+            this.sendBufferSize = sendBufferSize;
+            return this;
+        }
+
+        public Builder receiveBufferSize(int receiveBufferSize) {
+            this.receiveBufferSize = receiveBufferSize;
+            return this;
+        }
+
+        public Builder writeBufferHighWaterMark(int writeBufferHighWaterMark) {
+            this.writeBufferHighWaterMark = writeBufferHighWaterMark;
+            return this;
+        }
+
+        public Builder writeBufferLowWaterMark(int writeBufferLowWaterMark) {
+            this.writeBufferLowWaterMark = writeBufferLowWaterMark;
+            return this;
+        }
+
+        public TcpSettings build() {
+            if (connectTimeout == null) {
+                connectTimeout = Duration.ofSeconds(10);
+            }
+
+            if (closeTimeout == null) {
+                closeTimeout = Duration.ofMillis(500);
+            }
+
+            if (keepAlive == null) {
+                keepAlive = true;
+            }
+
+            if (tcpNoDelay == null) {
+                tcpNoDelay = true;
+            }
+
+            if (sendBufferSize == null) {
+                sendBufferSize = 65536;
+            }
+
+            if (receiveBufferSize == null) {
+                receiveBufferSize = 65536;
+            }
+
+            if (writeBufferHighWaterMark == null) {
+                writeBufferHighWaterMark = 65536;
+            }
+
+            if (writeBufferLowWaterMark == null) {
+                writeBufferLowWaterMark = 1024;
+            }
+
+            return new TcpSettings(this);
+        }
+    }
+
+    /*
+    // ir itraukti i pagrindini settings
+    //https://github.com/EventStore/EventStore/blob/release-v3.5.0/src/EventStore.ClientAPI/Transport.Tcp/TcpConfiguratin.cs
+    //.option(ChannelOption.SO_BACKLOG, settings.maxIncomingConnections)
+    //.option(ChannelOption.SO_KEEPALIVE, settings.keepAlive)
+    //.option(ChannelOption.TCP_NODELAY, settings.tcpNoDelay)
+    //.option(ChannelOption.SO_SNDBUF, settings.sendBufferSize)
+    //.option(ChannelOption.SO_RCVBUF, settings.receiveBufferSize)
+    .option(ChannelOption.WRITE_BUFFER_LOW_WATER_MARK, settings.writeBufferLowWaterMark)
+    .option(ChannelOption.WRITE_BUFFER_HIGH_WATER_MARK, settings.writeBufferHighWaterMark)
+    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, (int) settings.connectionTimeout.toMillis())
+    */
+}
