a7815cd9615291431ebd0556c63e2555a7a69795
Mindaugas Semys <mindaugas.semys@gmail.com>
2016-01-17 19:24:22 +0200

    maintenance: use 'EventStoreException' as non-abstract class

--- src/main/java/lt/msemys/esjc/operation/EventStoreException.java
+++ src/main/java/lt/msemys/esjc/operation/EventStoreException.java
@@ -3,7 +3,7 @@ package lt.msemys.esjc.operation;
 /**
  * @see <a href="https://github.com/EventStore/EventStore/blob/dev/src/EventStore.ClientAPI/Exceptions/EventStoreConnectionException.cs">EventStore.ClientAPI/Exceptions/EventStoreConnectionException.cs</a>
  */
-public abstract class EventStoreException extends RuntimeException {
+public class EventStoreException extends RuntimeException {
 
     public EventStoreException() {
     }
