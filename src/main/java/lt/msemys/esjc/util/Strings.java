7e0fbe3781dc813ffe3f141558c4f89ed1cf6ef2
Mindaugas Semys <mindaugas.semys@gmail.com>
2016-01-27 20:10:57 +0200

    maintenance: enriched string utilities

--- src/main/java/lt/msemys/esjc/util/Strings.java
+++ src/main/java/lt/msemys/esjc/util/Strings.java
@@ -1,9 +1,20 @@
 package lt.msemys.esjc.util;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class Strings {
+    public static final String EMPTY = "";
 
     public static boolean isNullOrEmpty(String string) {
         return string == null || string.isEmpty();
+    }
+
+    public static String newString(byte[] bytes) {
+        return (bytes == null || bytes.length == 0) ? EMPTY : new String(bytes, UTF_8);
+    }
+
+    public static String defaultIfEmpty(String string, String defaultString) {
+        return isNullOrEmpty(string) ? defaultString : string;
     }
 
 }
