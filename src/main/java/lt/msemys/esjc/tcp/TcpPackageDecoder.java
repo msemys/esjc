31fd9525568873ef4c3cb61222eef3ee4755f99c
Mindaugas Semys <mindaugas.semys@gmail.com>
2016-02-12 00:44:11 +0200

    maintenance: removed loggers from encoder/decoder

--- src/main/java/lt/msemys/esjc/tcp/TcpPackageDecoder.java
+++ src/main/java/lt/msemys/esjc/tcp/TcpPackageDecoder.java
@@ -3,14 +3,10 @@ package lt.msemys.esjc.tcp;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class TcpPackageDecoder extends MessageToMessageDecoder<ByteBuf> {
-
-    private static final Logger logger = LoggerFactory.getLogger(TcpPackageDecoder.class);
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
@@ -25,10 +21,6 @@ public class TcpPackageDecoder extends MessageToMessageDecoder<ByteBuf> {
             msg.getBytes(msg.readerIndex(), array, 0, length);
 
             tcpPackage = TcpPackage.of(array);
-        }
-
-        if (logger.isTraceEnabled()) {
-            logger.trace(tcpPackage.toString());
         }
 
         out.add(tcpPackage);
--- src/main/java/lt/msemys/esjc/tcp/TcpPackageEncoder.java
+++ src/main/java/lt/msemys/esjc/tcp/TcpPackageEncoder.java
@@ -2,8 +2,6 @@ package lt.msemys.esjc.tcp;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -11,14 +9,8 @@ import static io.netty.buffer.Unpooled.wrappedBuffer;
 
 public class TcpPackageEncoder extends MessageToMessageEncoder<TcpPackage> {
 
-    private static final Logger logger = LoggerFactory.getLogger(TcpPackageEncoder.class);
-
     @Override
     protected void encode(ChannelHandlerContext ctx, TcpPackage msg, List<Object> out) throws Exception {
-        if (logger.isTraceEnabled()) {
-            logger.trace(msg.toString());
-        }
-
         out.add(wrappedBuffer(msg.toByteArray()));
     }
 
