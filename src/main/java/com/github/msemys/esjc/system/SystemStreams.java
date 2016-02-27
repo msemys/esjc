0def7a4d7a8f4c14dd36b981f30897d011b49289
Mindaugas Semys <mindaugas.semys@gmail.com>
2016-02-27 12:40:38 +0200

    added: metastream prefix constant

--- src/main/java/com/github/msemys/esjc/system/SystemStreams.java
+++ src/main/java/com/github/msemys/esjc/system/SystemStreams.java
@@ -1,23 +1,27 @@
 package com.github.msemys.esjc.system;
 
+import static com.github.msemys.esjc.util.Preconditions.checkArgument;
+
 public class SystemStreams {
     public static final String STREAMS_STREAM = "$streams";
     public static final String SETTINGS_STREAM = "$settings";
     public static final String STATS_STREAM_PREFIX = "$stats";
+    public static final String METASTREAM_PREFIX = "$$";
 
     private SystemStreams() {
     }
 
     public static String metastreamOf(String streamId) {
-        return "$$" + streamId;
+        return METASTREAM_PREFIX + streamId;
     }
 
     public static boolean isMetastream(String streamId) {
-        return streamId.startsWith("$$");
+        return streamId.startsWith(METASTREAM_PREFIX);
     }
 
     public static String originalStreamOf(String metastreamId) {
-        return metastreamId.substring(2);
+        checkArgument(isMetastream(metastreamId), "'%s' is not metastream", metastreamId);
+        return metastreamId.substring(METASTREAM_PREFIX.length());
     }
 
 }
