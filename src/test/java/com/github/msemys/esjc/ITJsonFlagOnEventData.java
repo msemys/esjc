f17bc680a0a00ce4c5022e8f57451bf37153cbbc
Mindaugas Semys <mindaugas.semys@gmail.com>
2016-04-06 18:59:53 +0300

    added: integration tests

---
+++ src/test/java/com/github/msemys/esjc/ITJsonFlagOnEventData.java
@@ -0,0 +1,67 @@
+package com.github.msemys.esjc;
+
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ITJsonFlagOnEventData extends AbstractIntegrationTest {
+
+    @Override
+    protected EventStore createEventStore() {
+        return eventstoreSupplier.get();
+    }
+
+    @Test
+    public void setsJsonFlag() {
+        final String stream = generateStreamName();
+
+        eventstore.appendToStream(stream, ExpectedVersion.noStream(), asList(
+            EventData.newBuilder()
+                .type("some-type")
+                .jsonData("{\"some\":\"json\"}")
+                .jsonMetadata((String) null)
+                .build(),
+            EventData.newBuilder()
+                .type("some-type")
+                .jsonData((String) null)
+                .jsonMetadata("{\"some\":\"json\"}")
+                .build(),
+            EventData.newBuilder()
+                .type("some-type")
+                .jsonData("{\"some\":\"json\"}")
+                .jsonMetadata("{\"some\":\"json\"}")
+                .build()
+        )).join();
+
+        try (Transaction transaction = eventstore.startTransaction(stream, ExpectedVersion.any()).join()) {
+            transaction.write(asList(
+                EventData.newBuilder()
+                    .type("some-type")
+                    .jsonData("{\"some\":\"json\"}")
+                    .jsonMetadata((String) null)
+                    .build(),
+                EventData.newBuilder()
+                    .type("some-type")
+                    .jsonData((String) null)
+                    .jsonMetadata("{\"some\":\"json\"}")
+                    .build(),
+                EventData.newBuilder()
+                    .type("some-type")
+                    .jsonData("{\"some\":\"json\"}")
+                    .jsonMetadata("{\"some\":\"json\"}")
+                    .build()
+            )).join();
+            transaction.commit().join();
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+
+        StreamEventsSlice result = eventstore.readStreamEventsForward(stream, 0, 100, false).join();
+
+        assertThat(result.events.size(), is(6));
+        result.events.forEach(e -> assertTrue("Event #" + e.originalEvent().eventNumber, e.originalEvent().isJson));
+    }
+
+}
