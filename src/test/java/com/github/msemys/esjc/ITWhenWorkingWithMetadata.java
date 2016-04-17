ae76a9aed35f6895552d7fd10e433139575ad343
Mindaugas Semys <mindaugas.semys@gmail.com>
2016-04-17 14:56:16 +0300

    added: integration tests

---
+++ src/test/java/com/github/msemys/esjc/ITWhenWorkingWithMetadata.java
@@ -0,0 +1,30 @@
+package com.github.msemys.esjc;
+
+import com.github.msemys.esjc.util.EmptyArrays;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class ITWhenWorkingWithMetadata extends AbstractIntegrationTest {
+
+    @Override
+    protected EventStore createEventStore() {
+        return eventstoreSupplier.get();
+    }
+
+    @Test
+    public void getsMetadataForAnExistingStreamAndNoMetadataExists() {
+        final String stream = generateStreamName();
+
+        eventstore.appendToStream(stream, ExpectedVersion.noStream(), asList(newTestEvent())).join();
+
+        RawStreamMetadataResult metadata = eventstore.getStreamMetadataAsRawBytes(stream).join();
+        assertEquals(stream, metadata.stream);
+        assertFalse(metadata.isStreamDeleted);
+        assertEquals(-1, metadata.metastreamVersion);
+        assertEquals(EmptyArrays.EMPTY_BYTES, metadata.streamMetadata);
+    }
+
+}
