f6382248871315fb24f0b0189afc01b4c3f6cdb8
Andri Bremm <andri.bremm@senacor.com>
2018-06-27 22:12:22 +0200

    Fixed #32 by improving the UUIDConverter. The EventId (UUID) serialization was adapted to create the same String representation as the EventStore (UI and HTTP API).

--- src/test/java/com/github/msemys/esjc/ITReadEvent.java
+++ src/test/java/com/github/msemys/esjc/ITReadEvent.java
@@ -1,9 +1,14 @@
 package com.github.msemys.esjc;
 
+import io.netty.handler.codec.http.FullHttpRequest;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
+import static com.github.msemys.esjc.http.HttpClient.newRequest;
+import static io.netty.handler.codec.http.HttpMethod.POST;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 
@@ -91,6 +96,26 @@ public class ITReadEvent extends AbstractEventStoreTest {
         assertEquals(stream, result.stream);
         assertEquals(0, result.eventNumber);
         assertNotNull(result.event.originalEvent().created);
+    }
+
+    @Test
+    public void returnsCorrectEventId() throws ExecutionException, InterruptedException {
+        final String stream = generateStreamName();
+        final UUID eventId = UUID.randomUUID();
+
+        // write event via http API
+        String uri = "/streams/" + stream;
+        FullHttpRequest request = newRequest(POST, uri, "{}", "application/json", eventstoreCredentials);
+        request.headers().set("ES-EventId", eventId.toString());
+        request.headers().set("ES-EventType", "event0");
+        httpClient.send(request).get();
+
+        // read event via tcp
+        EventReadResult result = eventstore.readEvent(stream, 0, true).join();
+
+        // compare event IDs
+        assertEquals(EventReadStatus.Success, result.status);
+        assertEquals(eventId, result.event.originalEvent().eventId);
     }
 
     @Test
